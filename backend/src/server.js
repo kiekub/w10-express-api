ee995c2629928fdf91277abdca4d7e95f41928f1
inatbalthazar <inatbalthazar@gmail.com>
2026-09-04 15:38:48 +0800

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,162 @@
+import express from "express";
+import { users } from "./fakeDB/fakeUser.js";
+
+const app = express();
+
+app.use(express.json());
+
+// CRUD routes
+
+// Read users
+app.get("/users", (req, res) => {
+    try {
+        //console.log(req);
+        res.json(users);
+    } catch (err) {
+        next(err)
+    }
+
+    }
+);
+
+// Create users
+app.post("/users", (req, res) => {
+     try {
+        const { username, email, password } = req.body;
+
+        if(!username || !email || !password){
+            return res
+                .status("400")
+                .json({error: "username, email and password are required"});
+        }
+        
+        const highestId = users.reduce((max, user) => 
+            Math.max(max, Number(user.id)), 0,
+        );
+
+        const nextId = String(highestId + 1);
+
+        const newUser = {
+            id: nextId, 
+            username: username, 
+            email: email, 
+            password: password
+        };
+
+        users.push(newUser);
+
+        return res
+            .status(201)
+            .json(newUser);
+       
+            
+        } catch(err) {
+            next(err);
+        }
+                
+
+    }
+);
+
+// Update users
+app.put("/users/:id", (req, res, next) => {
+    try {
+    const user = users.find((u)=> u.id === req.params.id)
+
+    if (!user){
+        return res
+            .status(404)
+            .json({error: `User not found id: ${req.params.id}`});
+        }
+
+        const{username, email, password} = req.body
+
+        if (!username || !email || !password) {
+            return res
+                .status(400)
+                .json({error: "username, email and password are required!"});
+            }
+
+        //user updated
+        user.username = username;
+        user.email = email;
+        user.password = password;
+
+        return res.status(200).json(user);
+
+        } catch (err) {
+            next(err);
+        }
+    }
+);
+
+
+
+// Delete users 
+app.delete("/users/:id", (req, res) => {
+    try {
+           
+    // const user = users.find((u)=> u.id === req.params.id)
+
+    // if (!user){
+    //     return res
+    //         .status(404)
+    //         .json({error: `User not found id: ${req.params.id}`});
+    //     }
+
+    //     const{username, email, password} = req.body
+
+    //     if (!username || !email || !password) {
+    //         return res
+    //             .status(400)
+    //             .json({error: "username, email and password are required!"});
+    //         }
+
+    //     //user deleted
+    //     const deletedUser = req.params.id;
+    //     users.findIndex((u) => u.id === deletedUser)
+
+    //     users = users.filter((u)=> u.id !== req.params.id);
+    //     return res.status(200).json(user);
+
+    // Check if user ID is valid
+    const index = users.findIndex((u)=> u.id === req.params.id);
+    
+    // Validation
+    if(index === -1){
+        return res
+            .status(404)
+            .json({error: `User not found id: ${req.params.id}`});
+    }
+    
+    // Remove the user
+    const [deleted] = users.splice(index, 1);
+
+    return res.status(200).json(deleted);
+ 
+        } catch(err) {
+            next(err);
+        }
+
+
+    }   
+);
+
+
+// Centralized error handler
+app.use((err, req, res, next) => {
+    return res.status(500).json({
+        error: "Something went wrong on the server...",
+        message: err.message,
+
+    });
+    }
+); 
+
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+        console.log(`Server is running on PORT:${PORT} 🌏`);
+    }
+);
